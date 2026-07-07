16a22fe60bf6f80e4462644550b0aa3f978dba38
Hefaistoscod <nyctelios11@gmail.com>
2026-07-07 13:54:16 -0600

    Añade script de verificación UTF-8 griego/latín

---
+++ src/hola_mundo.py
@@ -0,0 +1,11 @@
+"""
+hola_mundo.py
+Verifica que la codificación UTF-8 funciona correctamente
+para caracteres griegos y latinos en el entorno de trabajo.
+"""
+
+saludo_griego = "Χαῖρε, ὦ κόσμε!"
+saludo_latino = "Salve, munde!"
+
+print(saludo_griego)
+print(saludo_latino)
